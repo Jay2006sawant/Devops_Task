659134101e740ccd68e05ef921700a2b639b96f4
Jay2006sawant <jay242902@gmail.com>
2025-05-31 15:50:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js CI/CD Demo!');
+  res.send('You’ve reached Jay’s containerized Node.js app, deployed with CI/CD!');
 });
 
 app.listen(PORT, () => {
